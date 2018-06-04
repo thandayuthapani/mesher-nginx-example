9bd9f49a7f751f0b8e5e77ff352681d1b3802675
thandayuthapani <thandayuthapani.s@huawei.com>
2018-06-04 18:01:39 +0530

    Added Request forwarder

--- config_env.sh
+++
@@ -1,4 +0,0 @@
-net_name=$(ip -o -4 route show to default | awk '{print $5}')
-SERVER_HOST=$(ifconfig $net_name | grep -E 'inet\W' | grep -o -E [0-9]+.[0-9]+.[0-9]+.[0-9]+ | head -n 1)
-
-sed -i s/"ServiceCenter"/"$SERVER_HOST"/ docker-compose.yml
